20450860a6dd6b16227483fb61f25e6e5c5246f1
yinghuzhu <yinghuzhu@gmail.com>
2025-07-23 10:03:11 +0800

    修复Next.js国际化配置警告
    将i18n.localeDetection设置为false，符合Next.js 14.2.30版本的要求

--- mediacraft-frontend/next-i18next.config.js
+++ mediacraft-frontend/next-i18next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'zh'],
-    localeDetection: true,
+    localeDetection: false,
   },
   localePath: './public/locales',
 };
